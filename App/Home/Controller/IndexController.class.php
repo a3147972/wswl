1034f4b5419cb0246340405639e07b3e72942c12
a3147972 <2387813033@qq.com>
2015-09-29 18:54:18 +0800

    修改搜索

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -96,4 +96,47 @@ class IndexController extends BaseController
             $this->error('error');
         }
     }
+
+    public function getClass()
+    {
+        $k = I('post.k');
+
+        if ($k) {
+            $map['keywords'] = array('like', '%'.$k.'%');
+            $class_list = D('MerchantClass')->_list($map);
+            $class_id_list = array_column($class_list, 'id');
+
+            $merchant_map['name'] = array('like', '%'.$k.'%');
+            $merchant_list = D('Merchant')->_list($merchant_map);
+            $merchant_class_id_list = array_column($merchant_list, 'class_id');
+            $merchant_class_id_list = array_unique($merchant_class_id_list);
+
+            $class_id_list = array_merge($class_id_list, $merchant_class_id_list);
+
+            $class_map['id'] = array('in', $class_id_list);
+
+            $class_list = D('MerchantClass')->_list($map);
+        } else {
+            $class_list = D('MerchantClass')->_list();
+        }
+
+        unset($map);
+         //查询分类下数量
+        $map['audit_status'] = 1;
+        $map['authorization_end_time'] = array('gt', now());
+
+        $merchant_count = D('Merchant')->where($map)->group('class_id')->field('class_id,count(id) as count')->select();
+
+        $merchant_count = array_column($merchant_count, 'count', 'class_id');
+
+        foreach ($class_list as $_k => $_v) {
+            $class_list[$_k]['count'] = isset($merchant_count[$_v['id']]) ? $merchant_count[$_v['id']] : 0;
+        }
+
+        if ($class_list) {
+            $this->success($class_list);
+        } else {
+            $this->error('没有此数据');
+        }
+    }
 }
