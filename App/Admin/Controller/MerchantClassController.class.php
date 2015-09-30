e1aa10c98479e7641ecb1471596254a4ec1b7c36
a3147972 <2387813033@qq.com>
2015-09-30 17:07:46 +0800

    no message

--- App/Admin/Controller/MerchantClassController.class.php
+++ App/Admin/Controller/MerchantClassController.class.php
@@ -5,6 +5,31 @@ use Admin\Controller\BaseController;
 
 class MerchantClassController extends BaseController
 {
+    public function index()
+    {
+        $page = I('page', 1);
+        $page_size = I('page_size', 10);
+        $order = I('order', '');
+
+        $model = D(CONTROLLER_NAME);
+
+        //查询值
+        $pk = $model->getPk();
+        $order = empty($order) ? $pk . ' asc' : $order;
+        $map = method_exists($this, '_filter') ? $this->_filter() : array();
+
+        //查询数据
+        $list = $model->_list($map, '', $order, $page, $page_size);
+        $count = $model->_count($map);
+
+        //分页处理
+        $page_list = $this->page($count, $page, $page_size);
+
+        $this->assign('page_list', $page_list);
+        $this->assign('count', $count);
+        $this->assign('list', $list);
+        $this->display();
+    }
     /**
      * 防止删除有商户的分类
      * @method _before_del
