ec7113a7e910520d5ec6daf88592eccc2209f7e6
a3147972 <2387813033@qq.com>
2015-10-01 12:07:50 +0800

    1

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-// 检测PHP环境
-if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-    die('require PHP > 5.3.0 !');
-}
-
 define('APP_PATH', './App/');
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);
 define('RUNTIME_PATH', './runtime/');
 
 require('./ThinkPHP/ThinkPHP.php');
